8edbba779409326c891b707a5b55d6d3dd6141f4
김정수 <zeeq125@gmail.com>
2023-12-13 14:48:19 +0900

    연산자 공부

---
+++ src/ex02/ComOperator.java
@@ -0,0 +1,13 @@
+package ex02;
+
+public class ComOperator {
+    public static void main(String[] args) {
+        System.out.println((3 == 4) + " ");
+        System.out.println((3 != 4) + " ");
+        System.out.println((3 > 4) + " ");
+        System.out.println((4 > 3) + " ");
+
+        System.out.println((3 == 3 && 4 == 7) + " ");
+        System.out.println((3 == 3 || 4 == 7) + " ");
+    }
+}
---
+++ src/ex02/Pizza.java
@@ -0,0 +1,11 @@
+package ex02;
+
+public class Pizza {
+    public static void main(String[] args) {
+        double area1 = 2 * 3.141592 * 20 * 20;
+        double area2 = 3.141592 * 30 * 30;
+        System.out.println("20cm 피자 면적 = " + area1);
+        System.out.println("30cm 피자 면적 = " + area2);
+        System.out.println((area1 > area2)? "20cm 두개":"30cm 한개");
+    }
+}
