f182a7c34e5cb3dbfd8caf45c502c61e9360caa0
김정수 <zeeq125@gmail.com>
2023-12-13 14:37:49 +0900

    heap 공부í

--- src/ex04/MemEx02.java
+++ src/ex04/MemEx02.java
@@ -1,11 +1,20 @@
 package ex04;
 
 class Person2{
-
+    int age = 10;
+    char gender = '남';
 }
 
 public class MemEx02 {
     public static void main(String[] args) {
+        System.out.println("1년지남");
+        System.out.println("2년지남");
+        System.out.println("3년지남");
 
+        Person2 p2 = new Person2();
+        System.out.println(p2.age);
+        System.out.println(p2.gender);
+
+        System.out.println("메인 종료");
     }
 }
