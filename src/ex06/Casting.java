4bfe34fd4708591a1683bddc2cfd9de0efb6df32
김정수 <zeeq125@gmail.com>
2023-12-26 10:04:43 +0900

    Casting

---
+++ src/ex06/Casting.java
@@ -0,0 +1,23 @@
+package ex06;
+
+class Parent {
+    void print(){
+        System.out.println("Parent 메소드 호출");
+    }
+}
+class Child extends Parent{
+    @Override
+    void print() {
+        System.out.println("Child 메소드 호출");
+    }
+}
+public class Casting {
+    public static void main(String[] args) {
+        // Child c = new Parent(); - 컴파일 오류
+        Parent p = new Child(); // 업캐스팅: 자식 객체를 부모 객체로 형변환
+        p.print();              // 동적 메소드 호출, 자식의 print() 호출
+
+        Child c = (Child)p; //다운캐스팅: 부모 객체를 자식 객체로 형변환
+        c.print();          // 메소드 오버라이딩, 자식 객체의 print() 호출
+    }
+}
