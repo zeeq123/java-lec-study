0c7802a51b79894d84ee51db7fae6b05e5aa8c5a
김정수 <zeeq125@gmail.com>
2023-12-26 11:41:04 +0900

    abstract 예시

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - 변수 (int, double, long, boolean, char)
 - 조건문 if
 - 반복문 while, for
+- 
---
+++ src/ex07/AbstractTest.java
@@ -0,0 +1,34 @@
+package ex07;
+
+abstract class Shape{
+    int x, y;
+    public void translate(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public abstract void draw();
+}
+
+class Rectangle extends Shape{
+    int width, height;
+    public void draw(){
+        System.out.println("사각형 그리기 메소드");
+    }
+}
+
+class Circle extends Shape{
+    int radius;
+    public void draw(){
+        System.out.println("원 그리기 메소드");
+    }
+}
+
+public class AbstractTest {
+    public static void main(String[] args) {
+     // Shape s1 = new Shape(); 추상 클래스로 객체를 생성할 수 없다.
+        Shape s2 = new Circle();
+        s2.draw();
+        Shape s3 = new Rectangle();
+        s3.draw();
+    }
+}
